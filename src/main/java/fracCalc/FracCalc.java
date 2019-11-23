11ed5b9d69acfc7c698d180b31b88f5ebb9c5815
s-liangb <s-liangb@53S50029725.it.bsd405.org>
2019-11-22 20:03:12 -0800

    checkpoint 2 turn in

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,16 +3,16 @@
  */
 
 // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
-	// test your code
-	// This function takes a String 'input' and produces the result
-	//
-	// input is a fraction string that needs to be evaluated. For your program, this
-	// will be the user input.
-	// e.g. input ==> "1/2 + 3/4"
-	//
-	// The function should return the result of the fraction after it has been
-	// calculated
-	// e.g. return ==> "1_1/4"
+// test your code
+// This function takes a String 'input' and produces the result
+//
+// input is a fraction string that needs to be evaluated. For your program, this
+// will be the user input.
+// e.g. input ==> "1/2 + 3/4"
+//
+// The function should return the result of the fraction after it has been
+// calculated
+// e.g. return ==> "1_1/4"
 
 package fracCalc;
 
@@ -22,35 +22,66 @@ public class FracCalc {
 
 	public static void main(String[] args) {
 		// TODO: Read the input from the user and call produceAnswer with an equation
-		
-		// instantiate scanner
+
 		Scanner console = new Scanner(System.in);
-		// take next line of input
 		String equation = console.nextLine();
-		// print output of method with the equation parameter
-		System.out.println(produceAnswer(equation));
-		// close scanner
+		while (!equation.equals("quit")) {
+			System.out.println(produceAnswer(equation));
+			equation = console.nextLine();
+		}
 		console.close();
 	}
 
-	
 	public static String produceAnswer(String input) {
 		// TODO: Implement this function to produce the solution to the input
-		
-		//instantiate operand variable
-		String secondOperand = "";
-		//cycle through characters in equation
-		for (int i = 0; i < input.length(); i++) {
-			//when the character in the equation is the final space, set the second operand variable to the substring following the final space
-			if (input.charAt(i) == ' ') {				
-				secondOperand = input.substring(i + 1);				
-			}
-		}
-		// method returns the second operand as a string
-		return secondOperand;
-	}
 
+		String firstOperand = "";
+		String operator = "";
+		String secondOperand = "";
+		String secondWhole = "";
+		String seocndNumerator = "";
+		String secondDenominator = "";
+		int split = input.indexOf(" ");
+
+		firstOperand = input.substring(0, split);
+		operator = input.substring(split + 1, split + 2);
+		secondOperand = input.substring(split + 3);
+		
+		String output = parseOperand(secondOperand);
+		
+		return output;
+	}
+	
 	// TODO: Fill in the space below with any helper methods that you think you will
-	// need
+	
+	public static String parseOperand(String firstOperand) {
+
+		int firstWhole;
+		int firstNumerator;
+		int firstDenominator;
+		int firstSplit1;
+		int firstSplit2;
+		
+		firstSplit1 = firstOperand.indexOf("_");
+		firstSplit2 = firstOperand.indexOf("/");
+
+		if (firstSplit1 != -1) {
+			firstWhole = Integer.parseInt(firstOperand.substring(0, firstSplit1));
+			firstNumerator = Integer.parseInt(firstOperand.substring(firstSplit1 + 1, firstSplit2));
+			firstDenominator = Integer.parseInt(firstOperand.substring(firstSplit2 + 1));
+		} else if (firstSplit2 != -1) {
+			firstWhole = 0;
+			firstNumerator = Integer.parseInt(firstOperand.substring(0, firstSplit2));
+			firstDenominator = Integer.parseInt(firstOperand.substring(firstSplit2 + 1));
+		} else {
+			firstWhole = Integer.parseInt(firstOperand);
+			firstNumerator = 0;
+			firstDenominator = 1;
+		}
+		
+		
+		String output = "whole:" + Integer.toString(firstWhole) + " numerator:" + Integer.toString(firstNumerator) + " denominator:" + Integer.toString(firstDenominator);
+		return output;
+	}
 
 }
